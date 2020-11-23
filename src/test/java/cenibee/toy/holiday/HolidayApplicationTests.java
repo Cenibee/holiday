f1969b20941071a3b6a6416e3198419b0b728120
Seong Jung Kim <ksj3452@naver.com>
2020-11-23 16:02:22 +0900

    스프링 부트 - 웹 / jpa 초기화

---
+++ src/test/java/cenibee/toy/holiday/HolidayApplicationTests.java
@@ -0,0 +1,13 @@
+package cenibee.toy.holiday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
