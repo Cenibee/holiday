cb426858caf23332eb592a24dc5535e7d17c3ea4
Seong Jung Kim <ksj3452@naver.com>
2020-11-24 14:58:21 +0900

    Revert "시작하기 코드들 추가해 본것 (참조 및 연습용)"
    This reverts commit 70f5573acd622c698561983a4fb2b7ceb4d78967.

--- src/main/java/cenibee/toy/holiday/HolidayApplication.java
+++ src/main/java/cenibee/toy/holiday/HolidayApplication.java
@@ -3,16 +3,9 @@ package cenibee.toy.holiday;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * CHECK1 - @SpringBootApplication is consist of
- * 	- @Configuration
- * 	- @EnableAutoConfiguration
- * 	- @ComponentScan
- */
 @SpringBootApplication
 public class HolidayApplication {
 
-	// CHECK2 - the main method uses Spring Boot's SpringApplication.run() to launch an application - no xml files such as web.xml!
 	public static void main(String[] args) {
 		SpringApplication.run(HolidayApplication.class, args);
 	}
